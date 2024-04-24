25775279c324688ea01abd7b01e1bc7a05e36777
MozartSoares <mozartarmstrong@gmail.com>
2024-04-24 17:07:18 -0300

    -Finished the bonus requirements

--- MealPriceCalculator.py
+++ MealPriceCalculator.py
@@ -1,3 +1,5 @@
+#In addition to the requirements, I've added the payment amount validation and the option to add a tip to the total amount
+
 def meal_price_calculator():
   child_meal = float(input("What is the price of a child's meal ?\n"))
   adult_meal = float(input("What is the price of an adult's meal ?\n"))
@@ -13,9 +15,24 @@ def meal_price_calculator():
 
   print(f'Sales tax: ${sales_tax} \nTotal: ${total}')
   
+  wants_tip = input("Do you want to add a tip to support the waiter?, (please answer with y/n) \n")
+  
+  if wants_tip == "y":
+    recommended_amount = round(total * 0.1,2)
+    print(f"The recommended tip amount is ${recommended_amount}, but feel free to include any other value you wish")
+    tip_amount = float(input("What is the tip amount ?\n"))
+    total = round(total + tip_amount,2)
+    print(f"Thanks for supporting the waiter, your new total with the tip is {total}")
+
   payment_amount = float(input("What is the payment amount ?\n"))
   change = round(payment_amount - total,2)
   
-  print(f"Your change: ${change}")
+  if payment_amount < total:
+    print("Insufficient funds. Please try again.")
+    meal_price_calculator()
+    return
+  
+  print(f"Your change is: ${change}")
+  print(f"Thanks for using our meal price calculator !")
 
 meal_price_calculator()
